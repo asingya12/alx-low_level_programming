69edea1fa87417b447d517c5408288fcd9c0b449
Asingya Marion <asingyamarion@gmail.com>
2022-09-10 05:02:17 -0700

    Task 5 commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet letters
+ *
+ * Return: Always 0 (Successs)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <10)
+	{
+		printf("%i', n);
+		++n;
+	}
+	putchar('\n');
+	return (0);
+}
