91a7e003db86702bb7989ecc3f9292c1dac228df
asingya12 <111114221+asingya12@users.noreply.github.com>
2022-09-13 22:30:18 +0300

    Task 2 commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that will print the alphabet 10 times
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i = 10;
+
+	for (i = 0; i < 10; i++)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			++ch;
+		}
+		_putchar('\n');
+	}
+}
